df60160ce3144c5513a1d7f205d053afdc34091d
Satinder Singh Saini <swv095@gmail.com>
2022-07-16 16:57:52 +0530

    Implemented useState hook

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputElement = () =>{
-    return <input placeholder="Type Some Text"/>
+    const [inputText, setInputText] = useState('');
+    return <div>
+        <input onChange={(e)=>setInputText(e.target.value)}
+    placeholder="Type Some Text"/><br/>
+    {inputText}
+    </div>
 }
 
 export default InputElement;
