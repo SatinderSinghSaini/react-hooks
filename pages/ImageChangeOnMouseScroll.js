e0a327bfd963ff553ad377a6b35342bd760c482a
Satinder Singh Saini <swv095@gmail.com>
2022-07-18 16:29:10 +0530

    Implemented useEffect to set document title

--- pages/ImageChangeOnMouseScroll.js
+++ pages/ImageChangeOnMouseScroll.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImageToggleOnMouseScroll from "../src/ImageToggleOnMouseScroll";
 
 const ImageChangeOnMouseScroll = () =>{
+    const [currentImage, setCurrentImage] = useState(null);
+    useEffect(()=>{
+        window.document.title = currentImage;
+        console.log(`Setting document title: ${currentImage}`);
+    });
     return [1,2,3,4,5].map(id => {
-        return <div key={id} >
+        return <div key={id} onMouseOver={()=>{
+            setCurrentImage(id);
+            console.log(`Image Id: ${id}`);
+        }} >
         <ImageToggleOnMouseScroll primaryImage="/static/images/colorImage.png" secondaryImage="/static/images/blackWhite/bwImage.png" />                
     </div>
     });
